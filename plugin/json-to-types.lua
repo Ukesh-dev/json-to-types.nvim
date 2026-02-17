bf6700e82e0468cd04249c624abbed564b3df606
wanger <310779211wym@gmail.com>
2026-02-17 21:52:26 +0800

    feat(cmd): use vim.ui.picker when command called without args

--- plugin/json-to-types.lua
+++ plugin/json-to-types.lua
@@ -1,18 +1,34 @@
 local json_to_types = require("json-to-types")
 local utils = require("json-to-types.utils")
 
+---@param on_choice fun(language: string)
+local function select_language(on_choice)
+  vim.ui.select(utils.language_map, {
+    prompt = "Select target language",
+  }, function(choice)
+    if not choice then
+      return
+    end
+    on_choice(choice)
+  end)
+end
+
 vim.api.nvim_create_user_command("ConvertJSONtoLang", function(args)
-  if args.args == "" then
-    vim.notify("Please provide a language name", vim.log.levels.ERROR)
-    return
-  end
   if not json_to_types.convertTypes then
     vim.notify("convertTypes function not found", vim.log.levels.ERROR)
     return
   end
-  json_to_types.convertTypes(args.args)
+
+  if args.args ~= "" then
+    json_to_types.convertTypes(args.args)
+    return
+  end
+
+  select_language(function(choice)
+    json_to_types.convertTypes(choice)
+  end)
 end, {
-  nargs = 1,
+  nargs = "?",
   complete = function()
     return utils.language_map
   end,
@@ -20,17 +36,21 @@ end, {
 })
 
 vim.api.nvim_create_user_command("ConvertJSONtoLangBuffer", function(args)
-  if args.args == "" then
-    vim.notify("Please provide a language name", vim.log.levels.ERROR)
-    return
-  end
   if not json_to_types.convertTypesBuffer then
     vim.notify("convertTypesBuffer function not found", vim.log.levels.ERROR)
     return
   end
-  json_to_types.convertTypesBuffer(args.args)
+
+  if args.args ~= "" then
+    json_to_types.convertTypesBuffer(args.args)
+    return
+  end
+
+  select_language(function(choice)
+    json_to_types.convertTypesBuffer(choice)
+  end)
 end, {
-  nargs = 1,
+  nargs = "?",
   complete = function()
     return utils.language_map
   end,
